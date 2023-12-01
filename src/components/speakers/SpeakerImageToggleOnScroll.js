57da6062e398fe998fb39f574c49f48057745dfa
Samuel Rodriguez <samuelyret@gmail.com>
2023-12-01 18:20:46 +0100

    Colorize SpeakerImageToggleOnScroll when scrolling.

--- src/components/speakers/SpeakerImageToggleOnScroll.js
+++ src/components/speakers/SpeakerImageToggleOnScroll.js
@@ -1,24 +1,48 @@
+import { useState, useRef, useEffect } from 'react';
 export default function SpeakerImageToggleOnScroll({
   imageUrl,
   alt,
-  thumbNail,
+  thumbNail
 }) {
-  const inView = false;
+  const imageRef = useRef(null);
+  const [inView, setInView] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const grayScale = inView ? "grayscale(0%)" : "grayscale(100%)";
+  const grayScale = inView ? 'grayscale(0%)' : 'grayscale(100%)';
+
+  function scrollHandler() {
+    setInView(isInView());
+  }
+
+  useEffect(() => {
+    setInView(isInView);
+    setIsLoading(false);
+    window.addEventListener('scroll', scrollHandler);
+    return () => {
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+
+  function isInView() {
+    const rect = imageRef.current.getBoundingClientRect();
+    return rect.top >= 0 && rect.bottom <= window.innerHeight;
+  }
+  const gif1x1Transparent =
+    'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
 
   return (
     <img
-      src={imageUrl}
+      src={isLoading ? gif1x1Transparent : imageUrl}
       alt={alt}
       width={thumbNail ? 50 : 200}
       height={thumbNail ? 50 : 200}
       className={
         thumbNail
-          ? "img-fluid rounded-start "
-          : "img-fluid rounded-start speaker-image"
+          ? 'img-fluid rounded-start '
+          : 'img-fluid rounded-start speaker-image'
       }
       style={{ filter: `${grayScale}` }}
+      ref={imageRef}
     />
   );
 }
