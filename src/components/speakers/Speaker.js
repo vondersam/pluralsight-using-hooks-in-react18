2d54c5447a095b955dff15e4391807e94e0ab5fa
Samuel Rodriguez <samuelyret@gmail.com>
2023-12-03 17:37:37 +0100

    useContext in Speaker detail component.

--- src/components/speakers/Speaker.js
+++ src/components/speakers/Speaker.js
@@ -1,17 +1,30 @@
 import SpeakerDetail from './SpeakerDetail';
-import { speakerList } from '../../../speakersData';
 import { useContext } from 'react';
 import { ThemeContext } from '../themecontexts/themeContext';
+import {
+  SpeakersDataContext,
+  SpeakersDataProvider
+} from '../themecontexts/SpeakersDataContext';
 
-export default function Speaker({ id }) {
+function Inner({ id }) {
   const { darkTheme } = useContext(ThemeContext);
+  const { speakerList, loadingStatus } = useContext(SpeakersDataContext);
   const speakerRec = speakerList?.find((rec) => rec.id === id);
 
+  if (loadingStatus === 'loading') return <div>Loading...</div>;
   return speakerRec ? (
     <div className={darkTheme ? 'theme-dark' : 'theme-light'}>
       <SpeakerDetail speakerRec={speakerRec} showDetails={true} />
     </div>
   ) : (
     <h2 className="text-danger">Speaker {id} not found</h2>
+  );
+}
+
+export default function Speaker(props) {
+  return (
+    <SpeakersDataProvider>
+      <Inner {...props} />
+    </SpeakersDataProvider>
   );
 }
